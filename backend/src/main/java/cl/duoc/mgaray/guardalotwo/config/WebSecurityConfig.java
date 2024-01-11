2629fe65802148ef8f207b558895822b5fddbc4c
Fernanda Garay <107312112+FernandaGarayN@users.noreply.github.com>
2024-01-10 22:32:30 -0300

    Add files via upload

--- backend/src/main/java/cl/duoc/mgaray/guardalotwo/config/WebSecurityConfig.java
+++ backend/src/main/java/cl/duoc/mgaray/guardalotwo/config/WebSecurityConfig.java
@@ -1,31 +1,28 @@
 package cl.duoc.mgaray.guardalotwo.config;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+  private final CorsConfigurationSource corsConfigurationSource;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     var permitAll = new String[] {"/products"};
-    var corsConfiguration = new CorsConfiguration();
-    corsConfiguration.setAllowedOrigins(List.of("*"));
-    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
-    corsConfiguration.setAllowedHeaders(List.of("*"));
 
-    http.cors(
-        httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(request -> corsConfiguration));
-    http.authorizeHttpRequests(
-            requests ->
-                requests.requestMatchers(permitAll).permitAll().anyRequest().authenticated())
-        .csrf(AbstractHttpConfigurer::disable);
+    http.authorizeHttpRequests(r ->
+            r.requestMatchers(permitAll).permitAll().anyRequest().authenticated())
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource));
     return http.build();
   }
 }
